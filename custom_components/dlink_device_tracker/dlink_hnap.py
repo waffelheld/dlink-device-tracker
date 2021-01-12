97117f6c9987e2e4763a751ed75f1a118a7e62dc
waffelheld <waffelheld@users.noreply.github.com>
2021-01-12 21:09:54 +0100

    fixed i/o warning

--- custom_components/dlink_device_tracker/dlink_hnap.py
+++ custom_components/dlink_device_tracker/dlink_hnap.py
@@ -1,10 +1,3 @@
-try:
-    from urllib.request import Request, urlopen
-    from urllib.error import URLError, HTTPError
-except ImportError:
-    # Assume Python 2.x
-    from urllib2 import Request, urlopen
-    from urllib2 import URLError, HTTPError
 import xml.etree.ElementTree as ET
 import hashlib
 import hmac
@@ -13,6 +6,11 @@ import logging
 import xmltodict
 from collections import defaultdict
 import pprint
+from homeassistant.helpers.aiohttp_client import (
+  async_create_clientsession,
+  async_get_clientsession,
+)
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,28 +22,19 @@ class DLinkHNAP(object):
     Class layout is inspired by @rkabadi (https://github.com/rkabadi) for the Edimax Smart plug.
     """
 
-    def __init__(self, ip, password, user="admin",
-                 use_legacy_protocol=False):
-        """
-        Create a new SmartPlug instance identified by the given URL and password.
-
-        :rtype : object
-        :param host: The IP/hostname of the SmartPlug. E.g. '192.168.0.10'
-        :param password: Password to authenticate with the plug. Located on the plug.
-        :param user: Username for the plug. Default is admin.
-        :param use_legacy_protocol: Support legacy firmware versions. Default is False.
-        """
+    def __init__(self, ip, password, hass, user="Admin"):
         self.ip = ip
         self.url = "http://{}/HNAP1/".format(ip)
         self.user = user
         self.password = password
-        self.use_legacy_protocol = use_legacy_protocol
         self.authenticated = None
-        if self.use_legacy_protocol:
-            _LOGGER.info("Enabled support for legacy firmware.")
         self._error_report = False
-        self.model_name = self.SOAPAction(Action="GetDeviceSettings", responseElement="ModelName", params="")
+        self.session = async_get_clientsession(hass)
+        #self.model_name = self.SOAPAction(Action="GetDeviceSettings", responseElement="ModelName", params="")
         
+    
+    
+    
     def requestBody(self, Action, params):
         """Returns the request payload for an action as XML>.
 
@@ -64,7 +53,12 @@ class DLinkHNAP(object):
         </soap:Body>
         </soap:Envelope>
                '''.format(Action, params, Action)
-
+    
+    @property
+    async def get_authenticated(self):
+        
+        return self.authenticated
+    
     async def SOAPAction(self, Action, responseElement, params="", recursive=False):
         """Generate the SOAP action call.
 
@@ -78,16 +72,7 @@ class DLinkHNAP(object):
         :param recursive: True if first attempt failed and now attempting to re-authenticate prior
         :return: Text enclosed in responseElement brackets
         """
-        # Authenticate client
-        if self.authenticated is None:
-            self.authenticated = self.auth()
         auth = self.authenticated
-        # If not legacy protocol, ensure auth() is called for every call
-        if not self.use_legacy_protocol:
-            self.authenticated = None
-
-        if auth is None:
-            return None
         payload = self.requestBody(Action, params)
 
         # Timestamp in microseconds
@@ -100,24 +85,13 @@ class DLinkHNAP(object):
                    'SOAPAction': '"http://purenetworks.com/HNAP1/{}"'.format(Action),
                    'HNAP_AUTH': '{}'.format(AUTHKey),
                    'Cookie': 'uid={}'.format(auth[1])}
+        _LOGGER.warning(headers)
 
-        try:
-            response = urlopen(Request(self.url, payload.encode(), headers))
-        except (HTTPError, URLError):
-            # Try to re-authenticate once
-            self.authenticated = None
-            # Recursive call to retry action
-            if not recursive:
-                return_value = self.SOAPAction(Action, responseElement, params, True)
-            if recursive or return_value is None:
-                _LOGGER.warning("Failed to open url to {}".format(self.ip))
-                self._error_report = True
-                return None
-            else:
-                return return_value
-        xmlData = response.read().decode()
-        
-        return xmltodict.parse(xmlData)
+        async with self.session.post(self.url, data=payload, headers=headers) as response:
+          #response = urlopen(Request(self.url, payload.encode(), headers))
+          xmlData = await response.text()
+          #_LOGGER.warning(xmlData)
+          return xmltodict.parse(xmlData)
         
     @property
     async def client_list(self):
@@ -138,7 +112,7 @@ class DLinkHNAP(object):
         return result
             
         
-    def auth(self):
+    async def auth(self):
         """Authenticate using the SOAP interface.
 
         Authentication is a two-step process. First a initial payload
@@ -160,58 +134,59 @@ class DLinkHNAP(object):
                    'SOAPAction': '"http://purenetworks.com/HNAP1/Login"'}
 
         # Request privatekey, cookie and challenge
-        try:
-            response = urlopen(Request(self.url, payload, headers))
-        except URLError:
-            if self._error_report is False:
-                _LOGGER.warning('Unable to open a connection to dlink switch {}'.format(self.ip))
+        
+        
+        async with self.session.post(self.url, data=payload, headers=headers) as response:
+          xmlData = await response.text()
+          #response = urlopen(Request(self.url, payload, headers))
+          #_LOGGER.warning(response.read().decode())
+          #_LOGGER.warning(response)
+          root = ET.fromstring(xmlData)
+
+          # Find responses
+          ChallengeResponse = root.find('.//{http://purenetworks.com/HNAP1/}Challenge')
+          CookieResponse = root.find('.//{http://purenetworks.com/HNAP1/}Cookie')
+          PublickeyResponse = root.find('.//{http://purenetworks.com/HNAP1/}PublicKey')
+
+          if (
+                  ChallengeResponse == None or CookieResponse == None or PublickeyResponse == None) and self._error_report is False:
+              _LOGGER.warning("Failed to receive initial authentication from smartplug.")
+              self._error_report = True
+              return
+
+          if self._error_report is True:
+              return
+
+          Challenge = ChallengeResponse.text
+          Cookie = CookieResponse.text
+          Publickey = PublickeyResponse.text
+
+          # Generate hash responses
+          PrivateKey = hmac.new((Publickey + self.password).encode(), (Challenge).encode(), digestmod=hashlib.md5).hexdigest().upper()
+          login_pwd = hmac.new(PrivateKey.encode(), Challenge.encode(), digestmod=hashlib.md5).hexdigest().upper()
+
+          response_payload = self.auth_payload(login_pwd)
+          # Build response to initial request
+          headers = {'Content-Type': '"text/xml; charset=utf-8"',
+                     'SOAPAction': '"http://purenetworks.com/HNAP1/Login"',
+                     'HNAP_AUTH': '"{}"'.format(PrivateKey),
+                     'Cookie': 'uid={}'.format(Cookie)}
+                     
+          async with self.session.post(self.url, data=response_payload, headers=headers) as response:
+            xmlData = await response.text()
+            root = ET.fromstring(xmlData)
+            #_LOGGER.warning(xmlData)
+
+            # Find responses
+            login_status = root.find('.//{http://purenetworks.com/HNAP1/}LoginResult').text.lower()
+
+            if login_status != "success" and self._error_report is False:
+                _LOGGER.error("Failed to authenticate with DLink Router {}".format(self.ip))
                 self._error_report = True
-            return None
-        xmlData = response.read().decode()
-        root = ET.fromstring(xmlData)
+                return
 
-        # Find responses
-        ChallengeResponse = root.find('.//{http://purenetworks.com/HNAP1/}Challenge')
-        CookieResponse = root.find('.//{http://purenetworks.com/HNAP1/}Cookie')
-        PublickeyResponse = root.find('.//{http://purenetworks.com/HNAP1/}PublicKey')
-
-        if (
-                ChallengeResponse == None or CookieResponse == None or PublickeyResponse == None) and self._error_report is False:
-            _LOGGER.warning("Failed to receive initial authentication from smartplug.")
-            self._error_report = True
-            return None
-
-        if self._error_report is True:
-            return None
-
-        Challenge = ChallengeResponse.text
-        Cookie = CookieResponse.text
-        Publickey = PublickeyResponse.text
-
-        # Generate hash responses
-        PrivateKey = hmac.new((Publickey + self.password).encode(), (Challenge).encode(), digestmod=hashlib.md5).hexdigest().upper()
-        login_pwd = hmac.new(PrivateKey.encode(), Challenge.encode(), digestmod=hashlib.md5).hexdigest().upper()
-
-        response_payload = self.auth_payload(login_pwd)
-        # Build response to initial request
-        headers = {'Content-Type': '"text/xml; charset=utf-8"',
-                   'SOAPAction': '"http://purenetworks.com/HNAP1/Login"',
-                   'HNAP_AUTH': '"{}"'.format(PrivateKey),
-                   'Cookie': 'uid={}'.format(Cookie)}
-        response = urlopen(Request(self.url, response_payload, headers))
-        xmlData = response.read().decode()
-        root = ET.fromstring(xmlData)
-
-        # Find responses
-        login_status = root.find('.//{http://purenetworks.com/HNAP1/}LoginResult').text.lower()
-
-        if login_status != "success" and self._error_report is False:
-            _LOGGER.error("Failed to authenticate with SmartPlug {}".format(self.ip))
-            self._error_report = True
-            return None
-
-        self._error_report = False  # Reset error logging
-        return (PrivateKey, Cookie)
+            self._error_report = False  # Reset error logging
+            self.authenticated = (PrivateKey, Cookie)
 
     def initial_auth_payload(self):
         """Return the initial authentication payload."""
